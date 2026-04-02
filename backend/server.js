56c4aa3722e50ad6924747cf8c798900bc1f5242
uknowme1802 <harshdeepsharma654@gmail.com>
2026-04-02 05:08:03 +0400

    updated server.js with cors

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
-const cors = require("cors");
+
 
 const app =express();
 connectDB();
