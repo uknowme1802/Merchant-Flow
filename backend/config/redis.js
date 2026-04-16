290d9f92368f41404d0e753f0667de3e5fd3811c
uknowme1802 <harshdeepsharma654@gmail.com>
2026-04-16 05:35:50 +0400

    Redis.js added with tls

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -1,6 +1,8 @@
 const Redis =  require("ioredis");
 
-const redis = new Redis(process.env.REDIS_URL);
+const redis = new Redis(process.env.REDIS_URL, {
+    tls:{},
+});
 
 redis.on("connect", ()=> console.log("✅ Redis connected"));
 redis.on("error", ()=> console.error("❌ Redis Error"));
