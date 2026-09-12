267979d96588c009dfb0b0b74f12deec2c3ad9b2
uknowme1802 <harshdeepsharma654@gmail.com>
2026-09-12 13:54:15 +0400

    API fix

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -61,7 +61,7 @@ export default function Login(){
   }
 
   try{
-    const res = await API.post("auth/2fa/verify-login", {
+    const res = await API.post("/auth/2fa/verify-login", {
       pendingToken, 
       token: code
     });
